ad1387ddc6a5f3685ba37292365e97aea769c73d
polinkavishinka <polinkavishinka@mail.ru>
2025-05-28 15:58:45 +0300

    fix: update navigation links to always point to home page

--- src/components/Header/Link.jsx
+++ src/components/Header/Link.jsx
@@ -3,7 +3,7 @@ import { HashLink } from 'react-router-hash-link';
 const Link = ({ link, title }) => {
   return (
     <li className="header__nav-item">
-      <HashLink smooth to={link} className="header__nav-link">
+      <HashLink smooth to={`/${link}`} className="header__nav-link">
         {title}
       </HashLink>
     </li>
